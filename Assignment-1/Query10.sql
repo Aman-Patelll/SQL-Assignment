de6a1a1b5b3d040dc85660e029bb6152ac8f8bc4
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-03 14:31:19 +0530

    Update Query10.sql

--- Assignment-1/Query10.sql
+++ Assignment-1/Query10.sql
@@ -1,8 +1,16 @@
--- BOPIS Orders Revenue (Last Year)  
+-- 10 BOPIS Orders Revenue (Last Year)
+-- Business Problem:
+-- BOPIS (Buy Online, Pickup In Store) is a key retail strategy. Finance wants to know the revenue from BOPIS orders for the previous year.
+
+-- Fields to Retrieve:
+
+-- TOTAL ORDERS
+-- TOTAL REVENUE
+
 SELECT  
     COUNT(OH.order_id) AS total_orders,  
     SUM(OH.grand_total) AS total_revenue  
 FROM order_header AS OH  
 JOIN order_item_ship_group AS OISG ON OH.order_id = OISG.order_id  
 WHERE OISG.shipment_method_type_id = 'STOREPICKUP'  
-AND OH.order_date BETWEEN '2024-02-11' AND '2025-02-11';  
+AND YEAR(OH.order_date) = YEAR(current_date()) - 1;  
