f2c70db9138d3e642dd40ddb14f8a78dc8715963
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-03 11:06:49 +0530

    Update Query1.sql

--- Assignment-1/Query1.sql
+++ Assignment-1/Query1.sql
@@ -1,4 +1,16 @@
--- New Customers Acquired in June 2023  
+-- 1 New Customers Acquired in June 2023
+-- Business Problem:
+-- The marketing team ran a campaign in June 2023 and wants to see how many new customers signed up during that period.
+
+-- Fields to Retrieve:
+
+-- PARTY_ID
+-- FIRST_NAME
+-- LAST_NAME
+-- EMAIL
+-- PHONE
+-- ENTRY_DATE
+
 SELECT  
     PER.party_id,  
     PER.first_name,  
@@ -10,8 +22,8 @@ SELECT
 FROM person AS PER  
 INNER JOIN party_role AS PTYRL ON PER.party_id = PTYRL.party_id  
 LEFT JOIN party_contact_mech AS PCM ON PER.party_id = PCM.party_id  
-LEFT JOIN contact_mech AS CM ON PCM.contact_mech_id = CM.contact_mech_id  
-LEFT JOIN telecom_number AS TN ON PCM.contact_mech_id = TN.contact_mech_id  
+JOIN contact_mech AS CM ON PCM.contact_mech_id = CM.contact_mech_id  
+JOIN telecom_number AS TN ON PCM.contact_mech_id = TN.contact_mech_id  
 WHERE PTYRL.role_type_id = 'CUSTOMER'  
 AND PER.created_stamp BETWEEN '2023-06-01' AND '2023-06-30'
-ORDER BY entry_date;  
+ORDER BY entry_date;
