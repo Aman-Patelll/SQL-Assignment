937439f3213fb44e677b0e8f7a744e68505ebfe7
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-03 13:08:52 +0530

    Update Query5.sql

--- Assignment-1/Query5.sql
+++ Assignment-1/Query5.sql
@@ -1,3 +1,23 @@
+-- 5 Completed Orders in August 2023
+-- Business Problem:
+-- After running similar reports for a previous month, you now need all completed orders in August 2023 for analysis.
+
+-- Fields to Retrieve:
+
+-- PRODUCT_ID
+-- PRODUCT_TYPE_ID
+-- PRODUCT_STORE_ID
+-- TOTAL_QUANTITY
+-- INTERNAL_NAME
+-- FACILITY_ID
+-- EXTERNAL_ID
+-- FACILITY_TYPE_ID
+-- ORDER_HISTORY_ID
+-- ORDER_ID
+-- ORDER_ITEM_SEQ_ID
+-- SHIP_GROUP_SEQ_ID
+
+
 SELECT  
     P.product_id,  
     P.product_type_id,  
