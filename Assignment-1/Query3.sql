211fd01044e2427e662a1f1005c9e8ebdad3e5c7
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-03 11:12:16 +0530

    Update Query3.sql

--- Assignment-1/Query3.sql
+++ Assignment-1/Query3.sql
@@ -1,10 +1,20 @@
--- Products Missing NetSuite ID  
+-- 3 Products Missing NetSuite ID
+-- Business Problem:
+-- A product cannot sync to NetSuite unless it has a valid NetSuite ID. The OMS needs a list of all products that still need to be created or updated in NetSuite.
+
+-- Fields to Retrieve:
+
+-- PRODUCT_ID
+-- INTERNAL_NAME
+-- PRODUCT_TYPE_ID
+-- NETSUITE_ID (or similar field indicating the NetSuite ID; may be NULL or empty if missing)
+    
 SELECT  
     P.product_id,  
     P.internal_name,  
     P.product_type_id,  
     GI.id_value AS netsuite_id  
 FROM product AS P  
-JOIN good_identification AS GI ON P.product_id = GI.product_id  
+LEFT JOIN good_identification AS GI ON P.product_id = GI.product_id  
 WHERE GI.good_identification_type_id = 'ERP_ID'  
 AND (GI.id_value IS NULL OR GI.id_value = '');  
