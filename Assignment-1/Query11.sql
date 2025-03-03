9f8b958464731d316309baa80726da650cfd4a4b
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-03 14:40:48 +0530

    Update Query11.sql

--- Assignment-1/Query11.sql
+++ Assignment-1/Query11.sql
@@ -1,8 +1,17 @@
--- Canceled Orders (Last Month)  
+-- 11 Canceled Orders (Last Month)
+-- Business Problem:
+-- The merchandising team needs to know how many orders were canceled in the previous month and their reasons.
+
+-- Fields to Retrieve:
+
+-- TOTAL ORDERS
+-- CANCELATION REASON
+    
 SELECT  
     COUNT(OS.order_id) AS total_orders,  
     OS.change_reason  
 FROM order_status AS OS  
 WHERE OS.status_id = 'ORDER_CANCELLED'  
-AND OS.status_datetime BETWEEN '2025-01-01' AND '2025-01-31'  
+AND month(OS.status_datetime) BETWEEN month(current_date)-1 AND month(current_date)
+AND year(os.status_datetime)=year(current_date)
 GROUP BY OS.change_reason;  
