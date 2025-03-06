7ee2075deacca71d91ab4d36664e95ad30ec8171
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 18:17:10 +0530

    Update Query4.sql

--- Assignment-3/Query4.sql
+++ Assignment-3/Query4.sql
@@ -1,9 +1,18 @@
--- Returns and Appeasements  
+-- 4 Returns and Appeasements
+-- Business Problem:
+-- The retailer needs the total amount of items, were returned as well as how many appeasements were issued.
+
+-- Fields to Retrieve:
+
+-- TOTAL RETURNS
+-- RETURN $ TOTAL
+-- TOTAL APPEASEMENTS
+-- APPEASEMENTS $ TOTAL
+    
 SELECT  
-    COUNT(DISTINCT RH.return_id) AS total_returns,  
-    SUM(RI.return_price * RI.return_quantity) AS returns_total,  
-    COUNT(RA.return_id) AS total_appeasements,  
-    SUM(RA.amount) AS appeasements_total  
-FROM return_header AS RH  
-LEFT JOIN return_item AS RI ON RH.return_id = RI.return_id  
-LEFT JOIN return_adjustment AS RA ON RH.return_id = RA.return_id;  
+    COUNT(RI.return_id) AS Total_Return,  
+    SUM(RI.return_price * RI.return_quantity) AS Return_Total,  
+    COUNT(A.return_id) AS Total_appeasement,  
+    SUM(A.amount) AS appeasement_Total  
+FROM return_item RI  
+LEFT JOIN return_adjustment A ON A.return_id = RI.return_id  AND A.return_adjustment_type_id = 'APPEASEMENT';
