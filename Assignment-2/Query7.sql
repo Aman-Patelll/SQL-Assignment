829a36db6ac4cf63092d90df5ec20998f7724702
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 16:01:57 +0530

    Update Query7.sql

--- Assignment-2/Query7.sql
+++ Assignment-2/Query7.sql
@@ -20,5 +20,5 @@ FROM order_header AS OH
 JOIN order_item_ship_group AS OISG ON OH.order_id = OISG.order_id  
 JOIN facility AS F ON OISG.facility_id = F.facility_id  
 JOIN facility_type FT ON FT.facility_type_id = f.facility_type_id
-WHERE OH.status_id NOT IN ('ORDER_COMPLETED', 'ORDER_CANCELLED')  AND FT.parent_type_id IN ("VIRTUAL_FACILITY", "PHYSICAL_STORE")
+WHERE OH.status_id NOT IN ('ORDER_COMPLETED', 'ORDER_CANCELLED')  AND FT.parent_type_id IN ("VIRTUAL_FACILITY", "PHYSICAL_STORE", "DISTRIBUTION_CENTER")
 ORDER BY OH.order_id;  
