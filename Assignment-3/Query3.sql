9332d8c88b34bb5f92ff1f980efbe8f7e7812a65
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 19:06:06 +0530

    Update Query3.sql

--- Assignment-3/Query3.sql
+++ Assignment-3/Query3.sql
@@ -7,18 +7,18 @@
 -- PARTY_ID
 -- FIRST_NAME
 
-SELECT  
-    F.facility_id,  
-    F.facility_name,  
-    COUNT(OISG.order_id) AS total_one_day_shipped_orders,  
-    CONCAT(MIN(OISG.estimated_ship_date), ' to ', MAX(OISG.estimated_ship_date)) AS reporting_period  
-FROM order_item_ship_group AS OISG  
-JOIN order_shipment AS OS ON OS.order_id = OISG.order_id  
-JOIN shipment AS S ON S.shipment_id = OS.shipment_id  
-JOIN facility AS F ON F.facility_id = OISG.facility_id  
-WHERE OISG.shipment_method_type_id = 'NEXT_DAY'  
-AND S.status_id = 'SHIPMENT_SHIPPED'  
-AND OISG.created_stamp BETWEEN  DATE_FORMAT(CURDATE() - INTERVAL 1 MONTH, '%Y-%m-01') AND DATE_FORMAT(CURDATE(), '%Y-%m-01')
-GROUP BY F.facility_id
-ORDER BY total_one_day_shipped_orders DESC  
-LIMIT 1; 
+SELECT
+    PER.first_name, 
+	RH.from_party_id  
+FROM return_item RI  
+JOIN return_header RH ON RI.return_id = RH.return_id  
+JOIN person PER ON PER.party_id = RH.from_party_id  
+WHERE (RH.return_date >= DATE_FORMAT(CURDATE() - INTERVAL 1 MONTH, '%Y-%m-01')  
+AND RH.return_date < DATE_FORMAT(CURDATE(), '%Y-%m-01'))  
+AND RI.order_id IN (  
+    SELECT order_id  
+    FROM return_item  
+    GROUP BY order_id  
+    HAVING COUNT(*) = 1  
+)  
+ORDER BY RH.from_party_id;
