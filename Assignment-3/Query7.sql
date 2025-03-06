87069a1903d45f5f32bdd1f301ec8f04d312a465
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 19:07:46 +0530

    Update Query7.sql

--- Assignment-3/Query7.sql
+++ Assignment-3/Query7.sql
@@ -1,4 +1,14 @@
--- Store with Most One-Day Shipped Orders (Last Month)  
+-- 7 Store with Most One-Day Shipped Orders (Last Month)
+-- Business Problem:
+-- Identify which facility (store) handled the highest volume of “one-day shipping” orders in the previous month, useful for operational benchmarking.
+
+-- Fields to Retrieve:
+
+-- FACILITY_ID
+-- FACILITY_NAME
+-- TOTAL_ONE_DAY_SHIP_ORDERS
+-- REPORTING_PERIOD
+
 SELECT  
     F.facility_id,  
     F.facility_name,  
@@ -10,8 +20,7 @@ JOIN shipment AS S ON S.shipment_id = OS.shipment_id
 JOIN facility AS F ON F.facility_id = OISG.facility_id  
 WHERE OISG.shipment_method_type_id = 'NEXT_DAY'  
 AND S.status_id = 'SHIPMENT_SHIPPED'  
-AND OISG.created_stamp BETWEEN DATE_FORMAT(CURDATE() - INTERVAL 1 MONTH, '%Y-%m-01')  
-    AND LAST_DAY(CURDATE() - INTERVAL 1 MONTH)  
+AND OISG.created_stamp BETWEEN  DATE_FORMAT(CURDATE() - INTERVAL 1 MONTH, '%Y-%m-01') AND DATE_FORMAT(CURDATE(), '%Y-%m-01')
 GROUP BY F.facility_id
 ORDER BY total_one_day_shipped_orders DESC  
 LIMIT 1;  
