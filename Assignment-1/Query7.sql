f0f5fe2713f1d7380ace5c72378e1425ed622f49
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 12:12:56 +0530

    Update Query7.sql

--- Assignment-1/Query7.sql
+++ Assignment-1/Query7.sql
@@ -13,10 +13,8 @@ SELECT DISTINCT
     OH.order_id,  
     OH.grand_total AS total_amount,  
     OPP.payment_method_type_id AS payment_method,  
-    OI.external_id AS shopify_id  
+    OH.external_id AS shopify_id  
 FROM order_header AS OH  
-JOIN order_item AS OI ON OH.order_id = OI.order_id  
-LEFT JOIN order_payment_preference AS OPP ON OPP.order_id = OH.order_id  
+JOIN order_payment_preference AS OPP ON OPP.order_id = OH.order_id  
 WHERE OH.status_id = 'ORDER_CREATED'  
 AND OH.order_type_id = 'SALES_ORDER';  
-
