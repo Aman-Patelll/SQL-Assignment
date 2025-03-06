d11e9d09607598c7824e2e17c0a34b8fc5ab8dc7
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 14:56:00 +0530

    Update Query3.sql

--- Assignment-2/Query3.sql
+++ Assignment-2/Query3.sql
@@ -18,8 +18,7 @@ SELECT
     PA.city,  
     PA.state_province_geo_id AS state  
 FROM order_item AS OI  
-JOIN product AS P ON P.product_id = OI.product_id  
-JOIN order_header AS OH ON OH.order_id = OI.order_id  
+JOIN product AS P ON P.product_id = OI.product_id   
 JOIN order_contact_mech AS OCM ON OCM.order_id = OI.order_id  
 JOIN postal_address AS PA ON PA.contact_mech_id = OCM.contact_mech_id  
 WHERE PA.city = 'New York'  
