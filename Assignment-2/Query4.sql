68ca2a8d9014f4c6870f034f9851f5d3e8cb7b24
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 15:07:08 +0530

    Update Query4.sql

--- Assignment-2/Query4.sql
+++ Assignment-2/Query4.sql
@@ -17,8 +17,8 @@ SELECT
     SUM(OH.grand_total) AS total_revenue,  
     CONCAT(MIN(OH.order_date), ' to ', MAX(OH.order_date)) AS date_range  
 FROM order_header AS OH  
-JOIN order_item AS OI ON OH.order_id = OI.order_id  
-JOIN order_item_ship_group AS OISG ON OI.order_id = OISG.order_id  
+JOIN order_item_ship_group AS OISG ON OH.order_id = OISG.order_id  
 JOIN facility AS F ON OISG.facility_id = F.facility_id  
+WHERE OH.status_id='ORDER_COMPLETED'
 GROUP BY F.facility_id, F.facility_name  
-ORDER BY total_revenue DESC;  
+ORDER BY total_revenue DESC; 
