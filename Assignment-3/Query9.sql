cd46722d0581737ce11ce24cffd7395e910fc7dc
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 19:37:51 +0530

    Update Query9.sql

--- Assignment-3/Query9.sql
+++ Assignment-3/Query9.sql
@@ -1,10 +1,19 @@
--- Total Facilities That Sell the Product  
+-- 9 Total Facilities That Sell the Product
+-- Business Problem:
+-- Retailers want to see how many (and which) facilities (stores, warehouses, virtual sites) currently offer a product for sale.
+
+-- Fields to Retrieve:
+
+-- PRODUCT_ID
+-- PRODUCT_NAME (or INTERNAL_NAME)
+-- FACILITY_COUNT (number of facilities selling the product)
+-- (Optionally) a list of FACILITY_IDs if more detail is needed
+
 SELECT  
     P.product_id,  
     P.product_name,  
-    COUNT(DISTINCT F.facility_id) AS facility_count  
+    COUNT(DISTINCT PF.facility_id) AS facility_count  
 FROM product AS P  
-JOIN inventory_item AS II ON P.product_id = II.product_id  
-JOIN facility AS F ON F.facility_id = II.facility_id  
+JOIN product_facility AS PF ON PF.product_id = P.product_id  
 GROUP BY P.product_id, P.product_name  
 ORDER BY facility_count DESC;  
