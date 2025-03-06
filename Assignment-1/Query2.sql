2b40c089edbd33cacede207549adfda996d1ce15
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-03-06 11:41:13 +0530

    Update Query2.sql

--- Assignment-1/Query2.sql
+++ Assignment-1/Query2.sql
@@ -13,6 +13,6 @@ SELECT
     P.product_type_id,  
     P.internal_name  
 FROM product AS P  
-INNER JOIN product_type AS PT ON P.product_type_id = PT.product_type_id  
+JOIN product_type AS PT ON P.product_type_id = PT.product_type_id  
 WHERE PT.is_physical = 'Y'  
 AND P.sales_discontinuation_date IS NULL;  
