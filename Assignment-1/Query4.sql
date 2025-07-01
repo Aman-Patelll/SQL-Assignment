c5a868baab08100ef2bc4623430b35d6680f8302
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-07-01 19:12:07 +0530

    Update JOIN for custom table

--- Assignment-1/Query4.sql
+++ Assignment-1/Query4.sql
@@ -9,21 +9,10 @@
 -- HOTWAX_ID
 -- ERP_ID or NETSUITE_ID (depending on naming)
 
-SELECT  
-    GI.product_id,  
-    MAX(CASE WHEN GI.good_identification_type_id = 'SHOPIFY_PROD_ID' THEN GI.id_value END) AS shopify_id,  
-    MAX(CASE WHEN GI.good_identification_type_id = 'ERP_ID' THEN GI.id_value END) AS netsuite_id,  
-    MAX(CASE WHEN GI.good_identification_type_id = 'HC_GOOD_ID_TYPE' THEN GI.id_value END) AS hotwax_id  
-FROM good_identification AS GI  
-WHERE GI.good_identification_type_id IN ('SHOPIFY_PROD_ID', 'ERP_ID', 'HC_GOOD_ID_TYPE')  
-GROUP BY GI.product_id;  
-
-SELECT  
-    P.product_id,  
-    GI.id_value AS NetSuitId,  
-    SP.shopify_product_id AS shopify_id  
+SELECT 
+    DISTINCT P.product_id AS Hotwax_id,
+    GI_ERP.id_value as NetsuitId,
+    GI_SHOPIFY.id_value as ShopifyId
 FROM product P
-LEFT JOIN good_identification GI ON P.product_id = GI.product_id  
-JOIN shopify_product SP ON SP.product_id = P.product_id  
-WHERE GI.id_value IS NOT NULL  
-AND GI.good_identification_type_id = 'ERP_ID';
+JOIN (SELECT id_value, product_id FROM good_identification GI WHERE GI.good_identification_type_id='ERP_ID') AS GI_ERP ON GI_ERP.product_id = P.product_id
+JOIN (SELECT id_value, product_id FROM good_identification GI WHERE GI.good_identification_type_id='SHOPIFY_PROD_ID') AS GI_SHOPIFY ON GI_SHOPIFY.product_id = P.product_id;
