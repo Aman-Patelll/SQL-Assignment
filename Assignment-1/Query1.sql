12ac1f20fdced8ef4f252703111ced8edf6de634
Aman-Patelll <136141050+Aman-Patelll@users.noreply.github.com>
2025-07-01 18:44:39 +0530

    Update sequence of joins

--- Assignment-1/Query1.sql
+++ Assignment-1/Query1.sql
@@ -18,9 +18,10 @@ SELECT
     CM.info_string AS email,
     TN.contact_number AS phone
 FROM person PER
+JOIN party PTY ON PTY.party_id=PER.party_id 
+    AND PTY.created_date BETWEEN '2023-06-01' AND '2023-07-01'
+JOIN party_role PR ON PR.party_id=PER.party_id 
+    AND PR.role_type_id='CUSTOMER'
 LEFT JOIN party_contact_mech PCM ON PCM.party_id = PER.party_id
 LEFT JOIN contact_mech CM ON CM.contact_mech_id = PCM.contact_mech_id
-LEFT JOIN telecom_number TN ON TN.contact_mech_id = CM.contact_mech_id 
-JOIN party PTY on PTY.party_id=PER.party_id 
-JOIN party_role PR on PR.party_id=PER.party_id
-where PTY.created_date BETWEEN '2023-06-01' AND '2023-07-01' and PR.role_type_id='CUSTOMER';
+LEFT JOIN telecom_number TN ON TN.contact_mech_id = CM.contact_mech_id;
